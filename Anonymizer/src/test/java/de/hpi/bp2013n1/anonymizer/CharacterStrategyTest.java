99d92a4a7317becbdd43f93069f664538f6476f3
Jakob Reschke <jakres@gmail.com>
2014-10-31 15:53:56 +0100

    add test for CharacterStrategy.transform

--- Anonymizer/src/test/java/de/hpi/bp2013n1/anonymizer/CharacterStrategyTest.java
+++ Anonymizer/src/test/java/de/hpi/bp2013n1/anonymizer/CharacterStrategyTest.java
@@ -23,9 +23,12 @@ package de.hpi.bp2013n1.anonymizer;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Before;
@@ -33,13 +36,23 @@ import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
 
+import de.hpi.bp2013n1.anonymizer.TransformationStrategy.FetchPseudonymsFailedException;
+import de.hpi.bp2013n1.anonymizer.db.TableField;
+import de.hpi.bp2013n1.anonymizer.shared.Rule;
+import de.hpi.bp2013n1.anonymizer.shared.TransformationKeyCreationException;
+import de.hpi.bp2013n1.anonymizer.shared.TransformationKeyNotFoundException;
+import de.hpi.bp2013n1.anonymizer.shared.TransformationTableCreationException;
+
 public class CharacterStrategyTest {
 	
 	private CharacterStrategy sut;
+	private Rule sampleRule = new Rule(new TableField("table", "column", "schema"), null, "PPK");
 
 	@Before
 	public void createStrategy() throws SQLException {
-		sut = new CharacterStrategy(null, null, null);
+		sut = new CharacterStrategy(mock(Anonymizer.class),
+				mock(Connection.class, RETURNS_MOCKS), 
+				mock(Connection.class, RETURNS_MOCKS));
 	}
 
 	@Test
@@ -55,6 +68,25 @@ public class CharacterStrategyTest {
 		}
 		assertThat("newly generated mappings should not be equal after trying for 2 seconds",
 				map, not(equalTo(previousMap)));
+	}
+	
+	@Test
+	public void testTransform() throws TransformationTableCreationException, FetchPseudonymsFailedException, TransformationKeyCreationException, TransformationKeyNotFoundException {
+		sut.setUpTransformation(sampleRule);
+		List<String> transformed = sut.transform("AAA", sampleRule, 
+				(ResultSetRowReader) null);
+		assertThat(transformed, hasSize(1));
+		String transformedString = transformed.get(0);
+		assertThat(transformedString.length(), is(3));
+		assertThat(transformedString.charAt(2), is('A'));
+		assertThat(transformedString.charAt(0),
+				equalTo(transformedString.charAt(1)));
+		char pseudonym = transformedString.charAt(0);
+		transformedString = sut.transform("AAA", sampleRule, 
+				(ResultSetRowReader) null).get(0);
+		assertThat(transformedString.charAt(0), equalTo(pseudonym));
+		assertThat(transformedString.charAt(1), equalTo(pseudonym));
+		assertThat(transformedString.charAt(2), is('A'));
 	}
 
 }
